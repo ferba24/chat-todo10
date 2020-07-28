25a131ef6e05c69e3c3dbf044e28d5653a8325c5
Abraham Morales <abrahammoralescalderon@gmail.com>
2020-07-28 12:18:53 -0500

    [FIX] Get session storage

--- app/Http/Middleware/ApiConnect.php
+++ app/Http/Middleware/ApiConnect.php
@@ -11,12 +11,8 @@ class ApiConnect{
         if(!$user){
             abort(404);
         }
-        $sessions = \DB::table('sessions')->get();
-		foreach($sessions as $s){
-			$payload = unserialize(base64_decode($s->payload));
-			if(isset($payload['user']) && $payload['user'] == $user ){
-                return $next($req);
-			}
-		}
+        if($req->session()->get('user') == $user){
+            return $next($req);
+        }
     }
 }
