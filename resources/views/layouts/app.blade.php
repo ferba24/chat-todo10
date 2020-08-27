1ba74c4c32c24e52717775146c633796a46e59a9
Abraham Morales <abrahammoralescalderon@gmail.com>
2020-08-27 12:31:44 -0500

    [DEV] Move tinymce file to js folder & delete unused code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,48 +26,14 @@
 	<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
 	<link href="{{ asset('css/style.css') }}" rel="stylesheet">
 	<link href="{{ asset('css/messages.css') }}" rel="stylesheet">
-	<script src="{{asset('tinymce.min.js?apiKey=r72gi40ebpxjpvsf9huxa66nnm5ykzsz4390dhb5wvea5yg4') }}"></script>
-	<script>
-		/*tinymce.execCommand('mceFocus',false,'text');
-		tinymce.init({
-			selector:'#text2',
-			setup: function (ed) {
-				ed.on('keydown',function(e) {
-					if(e.keyCode == 13){
-						alert("ENTER PRESSED");
-						e.preventDefault();
-					}
-				});
-			}
-		});*/
-	</script>
+	<script src="{{asset('js/tinymce.min.js?apiKey=r72gi40ebpxjpvsf9huxa66nnm5ykzsz4390dhb5wvea5yg4') }}"></script>
 </head>
 <body>
     <div id="app-vue" class="fill">
-		<!--<input type="hidden" name="user_id" id="user_id" value="{{ session('user') }}"/>-->
-		
 		<navbar-component :login_user="login_user" :sound_active="sound_active" v-on:sound_activesent="setSoundActive"></navbar-component>
 		
         @yield('content')
 		
-		<div class="modal fade" id="showModalRoomAdd" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-			<div class="modal-dialog modal-lg" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="exampleModalLabel">Rooms 2</h5>
-						<small>Rooms to users in the chat.<small>
-					</div>
-					<div class="modal-body">
-						<iframe src="{{ route('room.index') }}" style="width: 100%; height: 400px;" frameborder="0" ></iframe>
-					</div>
-					<div class="row">
-						<div class="col-md-12 text-right">
-							<a href="" class="btn btn-default" data-dismiss="modal">Close</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
     @include('private')
     </div>
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
